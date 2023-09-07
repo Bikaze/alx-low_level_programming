be0711e7207e5c8df3611ed08aefec863d7af8c3
Bikaze <clmntmugisha@gmail.com>
2023-09-07 10:59:18 -0700

    change

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: always return 0
+  */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
