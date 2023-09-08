04c48090e66ebf9feb36a39369f1bd14efd27906
Bikaze <clmntmugisha@gmail.com>
2023-09-08 07:55:52 -0700

    change

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+  *main - entry point of the program
+  *Return: always return 0
+  */
+int main(void)
+{
+	int i = 0;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
