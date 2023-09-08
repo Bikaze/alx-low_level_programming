2abdc4b3c7c69648fdc19a539147e7ff372c4021
Bikaze <clmntmugisha@gmail.com>
2023-09-08 09:01:39 -0700

    change

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+  *main - entry point of the program
+  *Return: always return 0
+  */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j > 0)
+				putchar(',');
+				putchar(' ');
+			putchar('0' + i);
+			putchar('0' + j);
+		}
+	}
+	return (0);
+}
