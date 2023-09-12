1e41a928edf692cf4c4714283f432f0667138acc
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-12 17:09:21 +0200

    change

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+/**
+  *print_last_digit - program function to give last digit of a number
+  *@n: will be holding a number to be checked
+  *Return: will return the last digit of a number
+  */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
