ede724da33ab09f1a886d260e79caa46da18b2e8
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-12 20:18:32 +0200

    change

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+  *printer - helper function
+  *@n: will be holding number to print to the standard output
+  */
+void printer(int  n)
+{
+	if (n % 100 != 0)
+	{
+		_putchar(n / 100 + '0');
+	}
+	if (n % 10 != 0)
+	{
+		_putchar(n / 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+}
+
+/**
+  *print_to_98 - program function to input number up to 98
+  *@n: will be holding a number to start with
+  */
+
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printer(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+		printer(98);
+	}
+	else
+	{
+		for (i = n; i < 98; i++)
+		{
+			if (n < 0)
+			{
+				_putchar('-');
+				printer(-1 * n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				printer(n);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		printer(98);
+	}
+}
