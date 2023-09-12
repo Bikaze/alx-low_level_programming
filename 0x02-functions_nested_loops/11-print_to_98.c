013fc5e1bdd0e2db5a861c89ddfc7257a9f1abbe
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-12 20:40:46 +0200

    change

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,10 @@ void printer(int  n)
 	if ((n / 100) > 0)
 	{
 		_putchar(n / 100 + '0');
-		n %= 100;
 	}
 	if ((n / 10) > 0)
 	{
-		_putchar(n / 10 + '0');
+		_putchar((n / 10) % 10 + '0');
 	}
 	_putchar(n % 10 + '0');
 }
