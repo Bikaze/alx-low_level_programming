3f53fb6ab872a87ec170b571e50cb78290da7315
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-12 18:38:37 +0200

    change

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *times_table - program function to give times table
+  */
+void times_table(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 9)
+		{
+			_putchar(i * j + '0');
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		_putchar(i * j + '0');
+		_putchar('\n');
+		i++;
+	}
+}
