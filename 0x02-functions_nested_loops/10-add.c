6015db1b40bdd2764977e57a47bb7bb3e37ae123
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-12 19:22:26 +0200

    change

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+  *add - program function to add two numbers
+  *@n: will be holding the first number
+  *@k: will be holding the second number
+  *Return: will return the sum
+  */
+int add(int n, int k)
+{
+	return (n + k);
+}
