3cdf3c520fa5dfb15bb19eb0d7cd6a6d18ce6941
Bikaze <clmntmugisha@gmail.com>
2023-09-13 01:42:24 -0700

    change

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+  *print_times_table - program function to give times table
+  *@n: dimension of the table
+  */
+void Print_times_table(int n)
+{
+	int i = 0;
+	int j;
+
+	while (i <= n)
+	{
+		j = 0;
+		while (j <= n)
+		{
+			if ((i * j) > 9)
+			{
+				_putchar((i * j) / 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+			}
+			_putchar((i * j) % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		if ((i * j) > 9)
+		{
+			_putchar((i * j) / 10 + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+		_putchar((i * j) % 10 + '0');
+		_putchar('\n');
+		i++;
+	}
+}
