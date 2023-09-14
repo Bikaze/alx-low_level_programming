f1f64d62919ca8735e7cf78e07756a8803992d2e
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-14 20:46:27 +0200

    change

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+  *mul - program function to multiply two numbers
+  *@a: will be holding the first number
+  *@b: will be holding the second number
+  *Return: will return the product
+  */
+int mul(int a, int b)
+{
+	return (n * k);
+}
