199b0e041f6138c9e01871786fd9f5b2ed473dd6
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-14 20:56:20 +0200

    change

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_numbers - program function
+  *Return: will always return nothing
+  */
+void print_numbers(void)
+{
+	char nbr = '0';
+
+	while (nbr <= '9')
+	{
+		_putchar(nbr);
+		nbr;
+	}
+	_putchar('\n');
+}
