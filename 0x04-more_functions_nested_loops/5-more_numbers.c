4a4bfd4365799528e4a0dee17beb63c811993895
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-14 23:13:21 +0200

    change

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *more_numbers - program function
+  *Return: will always return nothing
+  */
+void more_numbers(void)
+{
+	int j = 0;
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (j <= 14)
+		{
+			if ((j / 10) > 0)
+			{
+				_putchar(48 + (j / 10));
+			}
+			_putchar(48 +(j % 10));
+			j++;
+		}
+		i++;
+		j = 0;
+		_putchar('\n');
+	}
+}
