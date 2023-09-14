15e1c2be2b2c98ef8a9b260e180157957dfa71aa
Bikaze <clmntmugisha@gmail.com>
2023-09-14 11:17:10 -0700

    change

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+  *_islower - program function
+  *@c: will be holding a character to be turned into an int
+  *Return: will return 1 if lowercase and 0 otherwise
+  */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
