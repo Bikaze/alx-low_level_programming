08f453574758ed16a0fffb62d758871444a649ab
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-14 23:29:39 +0200

    change

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *print_line - program function
+  *@n: number of times to print '_'
+  *Return: will always return nothing
+  */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n && i > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
