59a41abf4e8e0cee12d5c3599b17704435a25749
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-15 00:19:53 +0200

    change

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  *print_diagonal - program function
+  *@n: number of times to print '\'
+  *Return: will always return nothing
+  */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= n && n > 0)
+	{
+		j = i;
+		while (j--)
+		{
+			_putchar(' ');
+		}
+		if (i != 0)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+		i++;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
