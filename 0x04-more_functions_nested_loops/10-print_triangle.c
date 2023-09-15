a049ff6e2983c25ab969f64ae6482054d8511da3
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-15 02:31:48 +0200

    change

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  *print_triangle - program function
+  *@size: dimension of triangle base
+  *Return: will always return nothing
+  */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
