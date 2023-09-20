0b681e54ad87a1a33172ab82071d68a9a513fdf9
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-20 17:19:21 +0200

    change

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+/**
+  *check - a helper function
+  *@c: character to be checked
+  *Return: will return an integer
+  */
+int check(char c)
+{
+	int i = 0;
+	char arr[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+	'(', ')', '{', '}'};
+	while (i < 13)
+	{
+		if (c == arr[i])
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/**
+  *cap_string - program function
+  *@a: will be holding the array to transform
+  *Return: will return a pointer to a character
+  */
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 97 && a[i] <= 122 && check(a[i - 1]))
+		{
+			a[i] -= 32;
+		}
+		i++;
+	}
+	return (a);
+}
