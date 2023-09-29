347736d7a830d9d73d8870870844fc7a376aa72f
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-29 21:29:15 +0200

    change

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  *main - entry for the program
+  *@argc: argument count
+  *@argv: argument vector
+  *Return: will return and integer
+  */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i][0]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
