2fa512440be6106e633e715975a7166d9b1a02fa
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-29 21:52:59 +0200

    change

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdbool.h>
+
+/**
+  *check - helper function
+  *@s: string
+  *Return: integer
+  */
+bool check(char *s)
+{
+	char* endptr;
+    	strtol(s, &endptr, 10);
+
+    	return (*endptr == '\0');
+}
+
 /**
   *main - entry for the program
   *@argc: argument count
   *@argv: argument vector
   *Return: will return and integer
   */
-
 int main(int argc, char *argv[])
 {
 	int i = 0;
@@ -21,7 +34,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i][0]) == 0)
+		if (!check(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
