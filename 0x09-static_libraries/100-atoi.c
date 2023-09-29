b5ea7c3950967f87dad83b090e810ebc0c77db14
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-29 19:41:27 +0200

    change

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,7 +1,29 @@
+#include "main.h"
 /**
-  *_atoi - function
-  *@s: parameter
-  *Return: return an integer
-  */
+ * _atoi - convert string to integer
+ *
+ * @s: the string
+ *
+ * Return: integer
+ */
 int _atoi(char *s)
-{}
+{
+ 	int sign = 1;
+	int i = 0;
+ 	unsigned int r = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+ 	{
+ 		if (s[i] == '-')
+ 			sign *= -1;
+		i++;
+ 	}
+ 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+ 	{
+ 		r = (r * 10) + (s[i] - '0');
+ 		i++;
+ 	}
+ 	r *= sign;
+ 	return (r);
+ }
+
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,7 +3,8 @@
 #include <unistd.h>
 
 /* Custom _putchar function */
-int _putchar(char c) {
-    return write(1, &c, 1);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
 
