abc7bc5e246330e818ec0bc29d5effba49521c94
Clement MUGISHA <clmntmugisha@gmail.com>
2023-09-29 20:48:08 +0200

    change

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *main - entry for the program
+  *@argc: argument count
+  *@argv: argument vector
+  *Return: will return and integer
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
