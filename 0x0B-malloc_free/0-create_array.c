6ca17ad3c45118de03c85388d24b2dccf652256b
Bikaze <clmntmugisha@gmail.com>
2023-10-02 08:37:37 -0700

    change

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *s = (void *)malloc(sizeof(char) * size);
 	unsigned int i = 0;
 
-	if (s == NULL)
+	if (s == NULL || size == 0)
 	{
 		return (NULL);
 	}
