d6a1382bfca11a0c09751a7daf6ca0c51f91de2a
Bikaze <clmntmugisha@gmail.com>
2023-10-02 10:37:11 -0700

    change

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+/**
+  *str_concat - join two strings
+  *@s1: string 1
+  *@s2: string 2
+  *Return: will return a pointer to a character
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i = 0;
+	int j = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != '\0' && s2[i] != '\0')
+		{
+			i += 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	s = (char *)malloc(sizeof(char) * (i + 1));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[j] != '\0')
+	{
+		s[j] = s1[j];
+		j++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		s[j] = s2[i];
+		j++;
+		i++;
+	}
+	return (s);
+}
