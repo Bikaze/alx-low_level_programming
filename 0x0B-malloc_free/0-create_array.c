8a13a22517c461319e01d54de20c381c48ee2f91
Bikaze <clmntmugisha@gmail.com>
2023-10-02 08:23:19 -0700

    change

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+  *create_array - create array of size "size" and  put char "c" in it
+  *@size: size of the array
+  *@c: character to put in the array
+  *Return: will return a pointer to a character
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *s = (void *)malloc(sizeof(char) * size);
+	unsigned int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
