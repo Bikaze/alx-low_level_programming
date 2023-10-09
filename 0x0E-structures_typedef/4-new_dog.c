2c0070e974dde28b6fc7fe44474eab67049bb7bc
Bikaze <clmntmugisha@gmail.com>
2023-10-09 23:48:03 +0200

    change

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
-		return NULL;
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
 	return (d);
 }
