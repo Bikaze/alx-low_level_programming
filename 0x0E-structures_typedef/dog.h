fd85ab2633faf2363529dcfd454ca0e425a7350b
Bikaze <clmntmugisha@gmail.com>
2023-10-09 21:02:26 +0200

    change

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _H
+#define _H
+/**
+  *struct dog - dog description
+  *@name: dog's name
+  *@age: dog's age
+  *@owner: dog's owner
+  *Description: simple description of a dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
