b23d9e105ba98e45344d115ed78817404be60391
Bikaze <clmntmugisha@gmail.com>
2023-10-09 21:16:04 +0200

    change

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+  *init_dog - initialize struct dog instance
+  *@d: pointer to a dog instance
+  *@name: dog's name
+  *@age: dog's age
+  *@owner: dog's owner
+  *Return: nothing
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
