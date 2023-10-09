e550823767f50c15beb0679bd64326223c60b8f9
Bikaze <clmntmugisha@gmail.com>
2023-10-09 23:00:33 +0200

    change

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,5 @@
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+#include <stdlib.h>
+#include "dog.h"
 /**
   *init_dog - initialize struct dog instance
   *@d: pointer to a dog instance
@@ -15,7 +11,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+	{}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
