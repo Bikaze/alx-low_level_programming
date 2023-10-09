4f9856a499986951185e81bac828617c541bdc8b
Bikaze <clmntmugisha@gmail.com>
2023-10-10 00:28:13 +0200

    change

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+  *free_dog - free struct dog instance
+  *@d: pointer to a dog_t instance
+  *Return: nothing
+  */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
