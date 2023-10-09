869f397d3fb4d2da8db8b0161e2eec4be4179def
Bikaze <clmntmugisha@gmail.com>
2023-10-09 23:42:39 +0200

    change

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  *new_dog - print struct dog instance
+  *@name: dog's name
+  *@age: dog's age
+  *@owner: dog's owner
+  *Return: pointer to dog_t
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		return NULL;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	return (d);
+}
