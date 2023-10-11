f62a1a0fd58c70618e85a540f8296b14a146fb2d
Bikaze <clmntmugisha@gmail.com>
2023-10-11 17:31:52 +0200

    change

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - program entry point
+  *@argc: argument count
+  *@argv: argument vector
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	char *code;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	code = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", code[i]);
+		}
+		printf("%02hhx ", code[i]);
+	}
+	return (0);
+}
