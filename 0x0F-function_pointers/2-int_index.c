dfabc532dbb6b2d7fa0b990b94dad50fe9fcfe94
Bikaze <clmntmugisha@gmail.com>
2023-10-11 14:43:06 +0200

    change

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
   *int_index - search for an integer
   *@array: pointer to an array
   *@size: size of the array
-  *@action: function to execute on each element of the array
+  *@cmp: function to execute on each element of the array
   *Return: nothing
   */
 
@@ -11,12 +11,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
 	if (size <= 0)
 	{
 		return (-1);
 	}
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 		{
