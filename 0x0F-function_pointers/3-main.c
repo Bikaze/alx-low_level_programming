59d5a22910ed0ebaeb10918aba017415e80a24df
Bikaze <clmntmugisha@gmail.com>
2023-10-11 15:08:01 +0200

    change

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,9 @@
 
 int main(int argc, char **argv)
 {
+	char *operator = argv[2];
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
 	int (*f)(int, int);
 	int i;
 
@@ -20,20 +23,19 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-			&& atoi(argv[3]) == 0)
+	if ((*operator == '/' || *operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	f = get_op_func(argv[2]);
+	f = get_op_func(operator);
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	i = f(atoi(argv[1]), atoi(argv[3]));
+	i = f(num1, num2);
 	printf("%d\n", i);
 	return (0);
 }
