d9acf0894a5e7151a1d05b1bd02d4c8d014abf0e
Bikaze <clmntmugisha@gmail.com>
2023-10-12 21:41:39 +0200

    change

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,56 +12,24 @@ void print_all(const char * const format, ...)
 	char c;
 	int i;
 	float f;
-<<<<<<< HEAD
-	char *sep = "", *fmt = (char *) format, *s;
-=======
-	char *sep = ", ";
-	char *fmt = (char *) format;
->>>>>>> parent of 5a475af (change)
+	char *sep = ", ", *fmt = (char *) format, *s;
 
 	va_start(ap, format);
-	/*if (format)
-	{*/
-		while (*fmt)
+	while (*fmt)
+	{
+		if (!*(fmt + 1))
+			sep = "";
+		switch (*(fmt++))
 		{
-<<<<<<< HEAD
-			switch (*(fmt++))
-			{
-				case 's':
-					s = va_arg(ap, char *);
-					if (s == NULL)
-					{
-						printf("%s(nil)", sep);
-						break;
-					}
-					printf("%s%s", sep, s);
-					break;
-				case 'c':
-					c = (char) va_arg(ap, int);
-					printf("%s%c", sep, c);
-					break;
-				case 'i':
-					i = va_arg(ap, int);
-					printf("%s%d", sep, i);
-					break;
-				case 'f':
-					f = (float) va_arg(ap, double);
-					printf("%s%f", sep, f);
-					break;
-			}
-			sep = ", ";
-=======
 			case 's':
 				s = va_arg(ap, char *);
 				if (s == NULL)
 				{
 					printf("(nil)%s", sep);
+					break;
 				}
-				else
-				{
-					printf("%s%s", s, sep);
-				}
-				break;	
+				printf("%s%s", s, sep);
+				break;
 			case 'c':
 				c = (char) va_arg(ap, int);
 				printf("%c%s", c, sep);
@@ -74,9 +42,7 @@ void print_all(const char * const format, ...)
 				f = (float) va_arg(ap, double);
 				printf("%f%s", f, sep);
 				break;
->>>>>>> parent of 5a475af (change)
 		}
-	/*}*/
 	va_end(ap);
 	printf("\n");
 }
