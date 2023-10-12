93e62089871857bde659a6abaf3c0bacb8133c64
Bikaze <clmntmugisha@gmail.com>
2023-10-12 21:33:09 +0200

    conflict

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,16 +8,23 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
+	char *s;
 	char c;
 	int i;
 	float f;
+<<<<<<< HEAD
 	char *sep = "", *fmt = (char *) format, *s;
+=======
+	char *sep = ", ";
+	char *fmt = (char *) format;
+>>>>>>> parent of 5a475af (change)
 
 	va_start(ap, format);
 	/*if (format)
 	{*/
 		while (*fmt)
 		{
+<<<<<<< HEAD
 			switch (*(fmt++))
 			{
 				case 's':
@@ -43,6 +50,31 @@ void print_all(const char * const format, ...)
 					break;
 			}
 			sep = ", ";
+=======
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+				{
+					printf("(nil)%s", sep);
+				}
+				else
+				{
+					printf("%s%s", s, sep);
+				}
+				break;	
+			case 'c':
+				c = (char) va_arg(ap, int);
+				printf("%c%s", c, sep);
+				break;
+			case 'i':
+				i = va_arg(ap, int);
+				printf("%d%s", i, sep);
+				break;
+			case 'f':
+				f = (float) va_arg(ap, double);
+				printf("%f%s", f, sep);
+				break;
+>>>>>>> parent of 5a475af (change)
 		}
 	/*}*/
 	va_end(ap);
