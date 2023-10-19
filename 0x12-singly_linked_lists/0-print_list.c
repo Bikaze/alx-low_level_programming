9a8818b9e5765ef34597ec87da9831ea0752352d
Bikaze <clmntmugisha@gmail.com>
2023-10-20 01:32:48 +0200

    change

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <stddef.h>
 /**
   *print_list - function to print a linked list
@@ -15,7 +14,7 @@ size_t print_list(const list_t *h)
 		return (0);
 	while (h != NULL)
 	{
-		if (h->str == 0)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,17 +2,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 /**
-  *struct word - x-tics of a word
-  *@str: the word itself
-  *@len: the length of the word
-  *@next: pointer to another word
-  *Description: This is a structure of a word pointing to another
-  */
-typedef struct word {
-	char *str;
-	int len;
-	struct word *next;
-}list_t;
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
