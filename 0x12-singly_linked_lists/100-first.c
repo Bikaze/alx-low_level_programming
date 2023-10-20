f5c54570285678849480c9e5356f4db7e04e7327
Bikaze <clmntmugisha@gmail.com>
2023-10-20 08:53:58 -0700

    change

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@ void first(void) __attribute__((constructor));
   *first - will execute before the main function
   *Return: nothing
   */
-void first (void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
