24aeb2e8b01babc87a97071933b262adde60ac5a
Bikaze <clmntmugisha@gmail.com>
2023-10-20 02:57:12 +0200

    change

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  *free_list - freeing a list
+  *@head: pointer to the start of the list
+  *Return: nothing
+  */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head-next;
+		free(head);
+		head = ptr;
+	}
+}
