35900c744096c8eccf071f79f4e1180a83676c83
Bikaze <clmntmugisha@gmail.com>
2023-10-23 15:03:21 -0700

    change

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *print_listint - print all elements of a list(structure)
+  *@h: a pointer to a listint_t list
+  *Return: count of printed elements
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h)
+	{
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
