39386fb3d24306455d916d695fb6bbcc12fa1d5d
Bikaze <clmntmugisha@gmail.com>
2023-10-23 15:11:20 -0700

    change

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+  *listint_len - count all elements of a list(structure)
+  *@h: a pointer to a listint_t list
+  *Return: count of printed elements
+  */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t i = 0;
+
+	if (h)
+	{
+		while (h)
+		{
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
