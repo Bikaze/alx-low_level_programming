4abc4ad6e819dde6f8446479bd6d8a1a8da461b2
Bikaze <clmntmugisha@gmail.com>
2023-10-25 19:08:58 +0200

    change

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
+int found(const listint_t *arr[], const listint_t *p);
 /**
   *print_listint_safe - print all elements of a list(structure)
-  *@h: a pointer to a listint_t list
+  *@head: a pointer to a listint_t list
   *Return: count of printed elements
   */
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
+	const listint_t *arr[1024];
 
-	if (h)
+	if (head)
 	{
-		while (h)
+		while (head && !found(arr, head))
 		{
-			printf("%d\n", h->n);
-			h = h->next;
+			arr[i] = head;
+			printf("[%p] %d\n",(void *)head, head->n);
+			head = head->next;
 			i++;
 		}
 	}
-	else 
-		return (98);
+	else
+		exit(98);
+	if (head)
+		printf("-> [%p] %d\n",(void *)head, head->n);
+
 	return (i);
+}
+
+/**
+  *found - look for already printed addresses
+  *@p: listint_t pointer
+  *@arr: array of listint_t pointers
+  *@Return: 0 or 1 (i.e false or true)
+  */
+
+int found(const listint_t *arr[], const listint_t *p)
+{
+	int i = 0;
+	
+	while (arr[i])
+	{
+		if (arr[i] == p)
+			return (1);
+		i++;
+	}
+	return (0);
 }
