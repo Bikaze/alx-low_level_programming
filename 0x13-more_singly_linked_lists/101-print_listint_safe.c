6f7cb623fd0311075e78024bce99074c87abb8d9
Bikaze <clmntmugisha@gmail.com>
2023-10-25 16:17:35 +0200

    change

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  *print_listint_safe - print all elements of a list(structure)
+  *@h: a pointer to a listint_t list
+  *Return: count of printed elements
+  */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+
+	if (h)
+	{
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
+	}
+	else 
+		return (98);
+	return (i);
+}
