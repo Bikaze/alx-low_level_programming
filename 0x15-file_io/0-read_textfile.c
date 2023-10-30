e1744b037d72715be5e9577f1e2ae2011dcd172c
Bikaze <clmntmugisha@gmail.com>
2023-10-31 00:19:26 +0200

    change

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+/**
+  *read_textfile - read text file and print it to stdout
+  *@filename: name of the file to read
+  *@letters: number of letters(chars) to read and write
+  *Return: the number of letters(chars) it read and printed
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_bytes;
+	char buffer[1024];
+	ssize_t written_bytes;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	read_bytes = read(fd, buffer, letters);
+
+	if (read_bytes == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	written_bytes = write(1, buffer, (size_t)read_bytes);
+
+	if (written_bytes == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+
+	return (written_bytes);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <stddef.h>
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
