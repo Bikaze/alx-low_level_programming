726977653a87452a03e4aadfff9f59914203b9f5
Bikaze <clmntmugisha@gmail.com>
2023-10-31 12:58:18 +0200

    change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (!text_content)
+		return (1);
+
 	while (text_content[i])
 	{
 		if (write(fd, &text_content[i], 1) == -1)
