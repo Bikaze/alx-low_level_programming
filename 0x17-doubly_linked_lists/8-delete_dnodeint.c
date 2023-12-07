94ba065b7d6a841deb4ec35304f97240666aa85b
bikaze <clmntmugisha@gmail.com>
2023-12-07 18:16:59 +0200

    change

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			else
 			{
 				ptr->prev->next = ptr->next;
-				ptr->next->prev = ptr->prev;
+				if (ptr->next != NULL)
+					ptr->next->prev = ptr->prev;
 				free(ptr);
 				return (1);
 			}
