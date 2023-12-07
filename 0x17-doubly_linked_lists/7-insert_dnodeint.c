0faff99dd64cb69f393e892ac666966ba2c3abd5
bikaze <clmntmugisha@gmail.com>
2023-12-07 16:52:10 +0200

    change

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,8 +33,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr = ptr->next;
 		index++;
 	}
-	if (index == idx)
-		add_dnodeint_end(h, n);
-	free(new);
 	return (NULL);
 }
