96aa48a5ca14ff1e6e41f96ed8d604049e185b88
bikaze <clmntmugisha@gmail.com>
2024-02-21 18:50:50 +0200

    Updated island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,20 +3,24 @@
 5-island_perimeter
 """
 
+
 def island_perimeter(grid):
-	"""grid: 2x2 interger nested list"""
-	if len(grid) == 0 or len(grid) > 100 or len(grid[0]) > 100:
-		return 0
-	perimeter = 0
-	for j in range(len(grid)):
-		for i in range(len(grid[0])):
-			if grid[j][i] == 1:
-				if i != 0 and grid[j][i-1] == 0:
-					perimeter += 1
-				if i != len(grid[0]) - 1 and grid[j][i+1] == 0:
-					perimeter += 1
-				if j != 0 and grid[j-1][i] == 0:
-					perimeter += 1
-				if j != len(grid) - 1 and grid[j+1][i] == 0:
-					perimeter += 1
-	return perimeter
+    """grid: 2x2 interger nested list"""
+
+    if len(grid) == 0 or len(grid) > 100 or len(grid[0]) > 100:
+        return 0
+
+    perimeter = 0
+    for j in range(len(grid)):
+        for i in range(len(grid[0])):
+            if grid[j][i] == 1:
+                if i != 0 and grid[j][i-1] == 0:
+                    perimeter += 1
+                if i != len(grid[0]) - 1 and grid[j][i+1] == 0:
+                    perimeter += 1
+                if j != 0 and grid[j-1][i] == 0:
+                    perimeter += 1
+                if j != len(grid) - 1 and grid[j+1][i] == 0:
+                    perimeter += 1
+
+    return perimeter
